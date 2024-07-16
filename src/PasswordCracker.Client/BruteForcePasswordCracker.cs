d0ef3506212b8bc319c8a0eb15609aad8e0a8dc6
Ben Dickman <Ben.Dickman@connellsgroup.co.uk>
2024-07-16 17:14:53 +0100

    Initial commit of noddy password combination generator

---
+++ src/PasswordCracker.Client/BruteForcePasswordCracker.cs
@@ -0,0 +1,48 @@
+﻿namespace PasswordCracker.Client;
+public static class BruteForcePasswordCracker
+{
+    private static readonly string _passwordCharacters = "aAbBcC";
+    //private static readonly string _passwordCharacters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ1234567890";
+
+    private static int _totalCombinations = 0;
+    private static HashSet<string> _possiblePasswords = new HashSet<string>();
+
+    public static void Crack(string password)
+    {
+        Process(_passwordCharacters.ToCharArray());
+
+        var matchedPassword = _possiblePasswords.First(p => p.Equals(password));
+
+        Console.WriteLine($"Password '{matchedPassword}' matched");
+    }
+
+    private static void Process(char[] characters)
+    {
+        for (int i = 1; i <= characters.Length; i++)
+        {
+            Generate(characters, i, string.Empty);
+        }
+
+        Console.WriteLine($"Total combinations: {_totalCombinations}");
+    }
+
+    private static void Generate(char[] passwordCharacters, int index, string partialPassword)
+    {
+        if (index == 0)
+        {
+            Console.WriteLine(partialPassword);
+            _possiblePasswords.Add(partialPassword);
+            _totalCombinations++;
+
+            return;
+        }
+
+        var length = passwordCharacters.Length;
+
+        for (int j = 0; j < length; j++)
+        {
+            var appended = partialPassword + passwordCharacters[j];
+            Generate(passwordCharacters, index - 1, appended);
+        }
+    }
+}
---
+++ src/PasswordCracker.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PasswordCracker.Client;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter a password:");
+        var password = Console.ReadLine();
+
+        BruteForcePasswordCracker.Crack(password);
+
+        Console.ReadLine();
+    }
+}
